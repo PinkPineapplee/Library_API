a0d8419291e6fff4901a51db93dd67ca1acd30c2
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-12-17 21:13:28 +0100

    Feat urls.py book path

--- library/urls.py
+++ library/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import BookInventoryCreateAPIView, BookShowUpdateDestroyAPIView
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+
 ]
--- library/views.py
+++ library/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+# List all books / create new book
+class BookInventoryCreateAPIView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer = BookSerializer
+
+# Get / Update/ Delete a single book
+class BookShowUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
