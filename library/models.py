9c0edf98e2e667f04ed728a8e0224e55d13e5b9d
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-12-17 20:43:08 +0100

    Add book model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True)
+    published_date = models.DateField()
+    copies_available = models.PostiveIntegerField(default = 1)
+
+def __str__(self):
+    return f"{self.title} by {self.author}"
+    
