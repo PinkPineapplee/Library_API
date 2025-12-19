8e48101794cfafc087f4184848187de17f3840c5
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-12-19 17:28:03 +0100

    Add views for CRUD operations

--- library/views.py
+++ library/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
-from .models import Book
-from .serializers import BookSerializer
+from .models import Book, User
+from .serializers import BookSerializer, UserSerializer
 
 # Create your views here.
 def index(request):
@@ -18,3 +18,12 @@ class BookInventoryCreateAPIView(generics.ListCreateAPIView):
 class BookShowUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class UserListCreateAPIView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer = UserSerializer
+
+class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer = UserSerializer
