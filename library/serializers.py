9ccf7321465c823690cdb2f960789295ebd29063
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-12-19 17:14:08 +0100

    Add User model and serializer

--- library/serializers.py
+++ library/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, User
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'author', 'isbn', 'published_date', 'copies_available']
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id', 'Username', 'Email','Date_of_Membership', 'Active_Status']
