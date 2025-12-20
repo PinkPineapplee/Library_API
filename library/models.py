954e1ee595d6436f9ee308f6e572651600dde837
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-12-20 16:22:36 +0100

    feat: make transaction models

--- library/models.py
+++ library/models.py
@@ -14,10 +14,21 @@ def __str__(self):
 
 class User(models.Model):
     ID = models.primarykey
-    Username = models.CharField(max_length= 255)
-    Email = models.CharField(max_length= 255)  
+    Username = models.CharField(max_length= 255, unique= True)
+    Email = models.CharField(max_length= 255, unique =True)  
     Date_of_Membership = models.DateField()
     Active_Status = models.BooleanField()  
 
 def __str__(self):
     return f"{self.Username}"  
+
+
+
+class Transactions(models.Model):
+     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+     student = models.ForeignKey(User, on_delete=models.CASCADE)
+     check_out_date = models.DateField()
+     return_date = models.DateField(null=True, blank=True)
+
+def __str__(self):
+    return f"{self.student}"    
